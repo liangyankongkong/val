5ee1c6bdc27a1a0e6bba717a361e686392418b69
ltt <ltt0300@163.com>
2024-09-21 20:29:51 +0800

    local

---
+++ cmake-build-debug/CMakeFiles/val.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/val.dir/main.cpp.obj"
+  "CMakeFiles/val.dir/main.cpp.obj.d"
+  "libval.dll.a"
+  "val.exe"
+  "val.exe.manifest"
+  "val.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/val.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
